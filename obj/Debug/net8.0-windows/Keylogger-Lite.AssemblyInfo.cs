fe75cee524ea83fa3b270577c9ced96803e80502
Oleksii <cheberiako2016@gmail.com>
2025-06-22 14:14:35 +0300

    Last commit in main

--- obj/Debug/net8.0-windows/Keylogger-Lite.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Keylogger-Lite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Keylogger-Lite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd35f5f3719ae84f2c04f50dbc9d9aef22d28f0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6e3461500608ed67726f9116973840fd3fd2899")]
 [assembly: System.Reflection.AssemblyProductAttribute("Keylogger-Lite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Keylogger-Lite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
